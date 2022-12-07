bf8534ebead7280bcd6ceac94e9b44da11b32a8a
sebuszqo <sekn.kn@gmail.com>
2022-12-07 14:25:12 +0100

    Day-01 First puzzle during advent of code counting calories :)

---
+++ day1.py
@@ -0,0 +1,19 @@
+
+# getting data
+with open("input-01.in") as file:
+    data = [i for i in file.read().strip().split("\n")]
+    # print(data)
+
+# dealing with sorting and summing calories
+elfSum = 0
+arrOfsum = [] 
+for i in data:
+    if i != '':
+        elfSum += int(i)
+    if i == '':
+        arrOfsum.append(elfSum)
+        elfSum = 0
+
+arrOfsum.sort(reverse=True)
+print(f'Answer to part 1 day-01: {arrOfsum[0]}')
+print(f'Answer to part 2 day-01: {sum(arrOfsum[:3])}')
