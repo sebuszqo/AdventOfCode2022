0b9e9c32170dad9eae00436f5e42f701454e34ab
sebuszqo <sekn.kn@gmail.com>
2022-12-15 21:20:24 +0100

    I made a files to next day of adventofcode 2022 ;)

---
+++ day-08.py
@@ -0,0 +1,13 @@
+# with open("input-08.in") as file:
+#     readedFile = [i for i in file.read().strip().split("\n")]
+
+# lenOfVisibleRows = len(readedFile[0])
+# lenOfVisibleColumns = len(readedFile)
+
+# print(lenOfVisibleColumns+lenOfVisibleRows)
+
+
+this = {"michal":5}
+
+if "michal" in this.keys():
+    print("jest")
