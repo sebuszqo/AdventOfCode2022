1659f60880361fa05bc6f8836713337c3fe5dd5c
sebuszqo <sekn.kn@gmail.com>
2022-12-19 10:46:26 +0100

    Day-10 part2 that was weird

--- day-10.py
+++ day-10.py
@@ -5,11 +5,13 @@ with open("./data-inputs/input-10.in") as file:
 def signal_strengths(cycles, x):
     return int(cycles * x)
 
-
+# variables to count etc.
 singal_strengths_sum = 0
 # array of cycles that we want to count
 cycle_array = [20, 60, 100, 140, 180, 220]
+# starts from 1
 x = 1
+# number of been cycles
 cycles = 0
 
 # commented lines are the another solution of this challange
@@ -33,6 +35,47 @@ for line in lines:
         
 
 # singal_strengths_sum = sum(list_x[cycle] * cycle for cycle in range(20, len(list_x), 40))
+
+
+# ---------- PART 2 ----------
+cur_X = 1
+op = 0 
+ans = 0
+
+row = 0
+column = 0
+
+X =[1] * 241
+
+for line in lines:
+    parts = line.split(" ")
+
+    if parts[0] == "noop":
+        op += 1
+        X[op] = cur_X
+
+    elif parts[0] == "addx":
+        V = int(parts[1])
+
+        X[op + 1] = cur_X
+        cur_X += V
+
+        op += 2
+        X[op] = cur_X
+
+ans = [[None] * 40 for _ in range(6)]
+
+
+for row in range(6):
+    for col in range(40):
+        conuter = row * 40 + col + 1
+        if abs(X[conuter - 1] - (col)) <= 1:
+            ans[row][col] = "##"
+        else:
+            ans[row][col] = "  "
+
+for row in ans:
+    print("".join(row))
 
 
 # ---------- ANSWERS ----------
